99ab2c480b53352e871f8c1c8fd50deea23f2f97
Pedro Manse <pedro.manse@dmk3.com.br>
2024-07-03 08:55:42 -0300

    example usage

--- main.go
+++ main.go
@@ -1,6 +1,46 @@
 package main
 
 import (
-	"town"
+	"fmt"
+	"town/town"
 )
 
+func InitDoubler() *town.Thread[int, int] {
+	t := town.New[int, int](140, 140)
+	double := func(input <-chan int, yield chan<- int, die chan struct{}) {
+		for {
+			select {
+			case i := <-input:
+				yield <- 2 * i
+			case <-die:
+				for i := range input {
+					yield <- 2*i
+				}
+				return
+			}
+		}
+	}
+	t.Exec(double)
+	return t
+}
+func InitSimpleDoubler() *town.Thread[int, int] {
+	t := town.New[int, int](140, 140)
+	double := func(input int) int {
+		return input*2
+	}
+	t.ExecSimple(double)
+	return t
+}
+
+func main() {
+	dblr := InitDoubler()
+	snd, _ := dblr.Act()
+	snd <- 100
+	snd <- 100
+	snd <- 100
+	snd <- 100
+	snd <- 100
+	for out := range dblr.Die() {
+		fmt.Println(out)
+	}
+}
